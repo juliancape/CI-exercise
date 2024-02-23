208314d384b7580b0388e6ec574afbaf4e174f35
juliancape <juliancape360@gmail.com>
2024-02-23 10:24:51 -0500

    Add format & pep8

--- utils_test.py
+++ utils_test.py
@@ -1,13 +1,17 @@
 from test import sum
 
+
 def test_sum_positivos():
     assert sum(2, 3) == 5
+
 
 def test_sum_negativos():
     assert sum(-2, -3) == -5
 
+
 def test_sum_positivo_negativo():
     assert sum(2, -3) == -1
+
 
 def test_sum_cero():
     assert sum(0, 0) == 0
